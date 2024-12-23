e538bc3b879c1ea1a5aa08e1e76253193d997416
Betiza Barreira <be.penques@hotmail.com>
2024-12-23 20:42:21 -0300

    componente: Box

--- packages/docs/src/stories/Button.stories.d.ts
+++
@@ -1,7 +0,0 @@
-import type { StoryObj, Meta } from '@storybook/react';
-import { ButtonProps } from '@ignite-ui/react';
-declare const _default: Meta<ButtonProps>;
-export default _default;
-export declare const Primary: StoryObj<ButtonProps>;
-export declare const Big: StoryObj<ButtonProps>;
-//# sourceMappingURL=Button.stories.d.ts.map
