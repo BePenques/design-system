d68f832f7ac42d89a877d0f96b02206cde214042
Betiza Barreira <be.penques@hotmail.com>
2024-12-23 17:01:56 -0300

    configurando turborepo

---
+++ packages/docs/src/stories/Button.stories.d.ts
@@ -0,0 +1,7 @@
+import type { StoryObj, Meta } from '@storybook/react';
+import { ButtonProps } from '@ignite-ui/react';
+declare const _default: Meta<ButtonProps>;
+export default _default;
+export declare const Primary: StoryObj<ButtonProps>;
+export declare const Big: StoryObj<ButtonProps>;
+//# sourceMappingURL=Button.stories.d.ts.map
