c454f45308a301a8a018256f881c71dbeff37adf
Betiza Barreira <be.penques@hotmail.com>
2025-01-02 17:13:04 -0300

    component button

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -2,6 +2,7 @@ export * from './components/Box'
 export * from './components/Text'
 export * from './components/Heading'
 export * from './components/Avatar'
+export * from './components/Button'
 // // import { colors } from '@ignite-ui/tokens'
 // import { styled } from './styles'
 
