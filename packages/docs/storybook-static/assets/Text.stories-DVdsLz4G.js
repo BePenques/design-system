92a1fd7cbb2afa8f1f329bb1e853841861e59190
Betiza Barreira <be.penques@hotmail.com>
2025-01-10 19:14:00 -0300

    add docs workflow

---
+++ packages/docs/storybook-static/assets/Text.stories-DVdsLz4G.js
@@ -0,0 +1,6 @@
+import{T as m}from"./index-BrfzqMbV.js";import"./index-RigO-4kf.js";import"./_commonjsHelpers-CqkleIqs.js";import"./index-D2MAbzvX.js";import"./jsx-runtime-BjgbQsUx.js";import"./index-DBOexwZ3.js";import"./index-BIm0odtm.js";const T={title:"Typography/Text",component:m,args:{size:"md",children:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem ipsa, unde aliquid eveniet id deserunt repellat officia natus consequuntur cupiditate?"},argTypes:{size:{options:["xxs","xs","sm","md","xl","lg","2xl","4xl","5xl","6xl","7xl","8xl","9xl"],control:{type:"inline-radio"}}}},r={},e={args:{children:"Strong text",as:"strong"}};var t,s,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(o=(s=r.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var a,i,n;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    children: 'Strong text',
+    as: 'strong'
+  }
+}`,...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const y=["Primary","CustomTag"];export{e as CustomTag,r as Primary,y as __namedExportsOrder,T as default};
