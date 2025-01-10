07fa169661449b109fac4c1900180953ab374cf3
Betiza Barreira <be.penques@hotmail.com>
2025-01-10 19:44:26 -0300

    fix: add subpath to storybook on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -25,6 +25,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
+  viteFinal: (config, {configType})=>{
+    if(configType === 'PRODUCTION'){
+      config.base = '/design-system/'
+    }
+    return config;
+  }
 };
 
 
